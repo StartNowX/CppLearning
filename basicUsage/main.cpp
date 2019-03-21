274684f97b11d5e6bcf84829f8e79568dcaa3afc
shawn <shawnsun93@gmail.com>
2019-03-22 01:47:08 +0800

    enum in class

--- basicUsage/main.cpp
+++ basicUsage/main.cpp
@@ -8,9 +8,14 @@ int main(int argc, char* argv[]){
     person1.SetName("Jack Ma");
     person1.GetName();
 
-    const PersonManage person2("Jack Ma", 58, 170);
-    person2.SetName(name);
+    const PersonManage person2("Tom Ma", 58, 170);
+    person2.SetName("Yelo");
     person2.GetName();
+
+    //enum egg {SMALL, MEDIUM, LARGER};
+    //enum t_shirt {SMALL, MEDIUM, LARGER}; // error, redeclaration
+    enum class egg {SMALL, MEDIUM, LARGER};
+    enum class t_shirt {SMALL, MEDIUM, LARGER}; // use class/struct
 
     return 1;
 }
