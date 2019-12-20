296d3d5c1583ce4832b61c5734c64f79bc97ed98
sunxiang <sunxiang@fabu.ai>
2019-12-20 10:23:10 +0800

    feat(unique_ptr): update unique_ptr infos

--- notes/code/C++11/smart_pointer.cc
+++ notes/code/C++11/smart_pointer.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 
+
 void SharedPtr() {
     // create shared_ptr
     {
@@ -124,18 +125,19 @@ void SharedPtr() {
         std::cout << "*a = " << *a << ", *b = " << *b << std::endl;  // 20 10
         std::cout << "a.get() = " << a.get() << ", b.get() = " << b.get() << std::endl;
         std::cout << "comparing a and b...\n" << std::boolalpha;
-        std::cout << "value-based: " << (!(a.get() < b.get()) && !(b.get() < a.get())) << '\n';  // false, stored pointer
-        std::cout << "value-based: " << (!(a < b) && !(b < a)) << '\n';  // false, stored pointer
-        std::cout << "owner-based: " << (!a.owner_before(b) && !b.owner_before(a)) << '\n'; // true
+        std::cout << "value-based: " << (!(a.get() < b.get()) && !(b.get() < a.get()))
+                  << '\n';                                                                   // false, stored pointer
+        std::cout << "value-based: " << (!(a < b) && !(b < a)) << '\n';                      // false, stored pointer
+        std::cout << "owner-based: " << (!a.owner_before(b) && !b.owner_before(a)) << '\n';  // true
 
         delete p;
 
         std::shared_ptr<int> p10(new int(5));
         std::shared_ptr<int> p11(new int(30));
         std::cout << "p10.get() = " << p10.get() << ", p11.get() = " << p11.get() << std::endl;
-        std::cout << "p10.get() < p11.get() = " << (p10.get() < p11.get()) << std::endl; // false
-        std::cout << "p10 < p1 = " << (p10 < p11) << std::endl; // false
-        std::cout << "p10.owner_before(p11) = " << p10.owner_before(p11) << std::endl; // true
+        std::cout << "p10.get() < p11.get() = " << (p10.get() < p11.get()) << std::endl;  // false
+        std::cout << "p10 < p1 = " << (p10 < p11) << std::endl;                           // false
+        std::cout << "p10.owner_before(p11) = " << p10.owner_before(p11) << std::endl;    // true
     }
 
     std::cout << std::endl;
@@ -143,19 +145,159 @@ void SharedPtr() {
 
 void WeakPtr() { std::cout << std::endl; }
 
-void UniquePtr() { std::cout << std::endl; }
+
+class StateDeleter {
+   public:
+    StateDeleter() : count_(0) {}
+    template <typename T>
+    void operator()(T *p) {
+        std::cout << "[deleted #" << ++count_ << "]" << std::endl;
+        delete p;
+    }
+
+   private:
+    int count_;
+};
+
+void UniquePtr() {
+    // initialize
+    {
+        std::default_delete<int> ud;
+        std::unique_ptr<int> u1;
+        std::unique_ptr<int> u2(nullptr);
+        std::unique_ptr<int> u3(new int);
+        std::unique_ptr<int> u4(new int(3), ud);
+        std::unique_ptr<int> u5(new int(3), std::default_delete<int>());
+        std::unique_ptr<int> u6(std::move(u5));
+        std::unique_ptr<int> u7(std::move(u6));
+        // std::unique_ptr<int> u8(std::auto_ptr<int>(new int)); // auto_ptr is deprecated
+        // std::unique_ptr<int> u9 = u1; // error, unique_ptr delete copy construction
+
+        std::cout << "u1: " << (u1 ? "not null" : "null") << std::endl;  // null
+        std::cout << "u2: " << (u2 ? "not null" : "null") << std::endl;  // null
+        std::cout << "u3: " << (u3 ? "not null" : "null") << std::endl;  // not null
+        std::cout << "u4: " << (u4 ? "not null" : "null") << std::endl;  // not null
+        std::cout << "u5: " << (u5 ? "not null" : "null") << std::endl;  // not null, when u6, null
+        std::cout << "u6: " << (u6 ? "not null" : "null") << std::endl;  // not null, when u7, null
+        std::cout << "u7: " << (u7 ? "not null" : "null") << std::endl;  // not null
+        // std::cout << "u8: " << (u8 ? "not null" : "null") << std::endl; // not null
+    }
+
+    // assign
+    {
+        // reset
+        std::unique_ptr<int> u1;
+        std::unique_ptr<int> u2(new int);
+        std::cout << "u2: " << (u2 ? "not null" : "null") << std::endl;  // not null
+        u2.reset();
+        std::cout << "u2: " << (u2 ? "not null" : "null") << std::endl;  // null
+
+        u2.reset(new int(2));
+        std::cout << "u2: " << (u2 ? "not null" : "null") << std::endl;  // not null
+
+        // release
+        std::unique_ptr<int> u3(new int(3));
+        if (u3) {
+            std::cout << "*u3 = " << *u3 << std::endl;
+        } else {
+            std::cout << "u3 is null." << std::endl;
+        }
+
+        int *tmp = u3.release();  // return value and reset u3 with nullptr
+        if (u3) {
+            std::cout << "*u3 = " << *u3 << std::endl;
+        } else {
+            std::cout << "u3 is null." << std::endl;
+            std::cout << "*tmp = " << *tmp << std::endl;
+        }
+        delete tmp;
+
+        // swap
+        std::unique_ptr<int> u4;
+        // std::unique_ptr<int> u4(new int(2));
+        std::unique_ptr<int> u5(new int(5));
+        u5.swap(u4);
+        if (u5) {
+            std::cout << "*u5 = " << *u5 << std::endl;
+        }
+        if (u4) {
+            std::cout << "*u4 = " << *u4 << std::endl;
+        }
+    }
+
+    // get()
+    {
+        std::unique_ptr<int> u1(new int(10));
+        auto *auto_u1 = u1.get();
+        std::cout << "*auto_u1 = " << *auto_u1 << ", *u1 = " << *u1 << std::endl;
+
+        // 区分get()和release()，get()只是返回其stored
+        // pointer，但不释放对其对象的所有权，即调用get()后，原始unique_ptr仍不为空，且返回值不能用于构造其他管理指针
+        // release()则会释放对其对象的所有权
+    }
+
+    // get_deleter
+    {
+        // default deleter
+        std::unique_ptr<int> u1;
+
+        // alpha/beta use independent copies of the deleter
+        std::unique_ptr<int, StateDeleter> alpha(new int);
+        std::unique_ptr<int, StateDeleter> beta(new int, alpha.get_deleter());
+        // if stop here, alpha/beta will call their deconstructor function, print:
+        // [deleted #1]
+        // [deleted #1]
+
+        StateDeleter del;
+        // deleter type is reference, thus gamma/delta share the deleter "del"
+        std::unique_ptr<int, StateDeleter &> gamma(new int, del);
+        std::unique_ptr<int, StateDeleter &> delta(new int, gamma.get_deleter());
+
+        std::cout << "calling reset alpha...";
+        alpha.reset(new int);
+        std::cout << "calling reset beta...";
+        beta.reset(new int);
+        std::cout << "calling reset gamma...";
+        gamma.reset(new int);
+        std::cout << "calling reset delta...";
+        delta.reset(new int);
+        // if stop here, alpha/beta will also call their deconstructor function, print:
+        // calling reset alpha...[deleted #1]
+        // calling reset beta...[deleted #1]
+        // calling reset gamma...[deleted #1]
+        // calling reset delta...[deleted #2] // 注意这里+1，因为delta和gamma共享deleter
+        // [deleted #3]
+        // [deleted #4]
+        // [deleted #2]
+        // [deleted #2]
+
+        std::cout << "calling gamma/delta deleter...";
+        gamma.get_deleter()(new int);
+
+        // a brand new deleter for alpha
+        alpha.get_deleter() = StateDeleter();
+    }
+
+    {
+        std::cout << "shared deleter " << std::endl;
+        std::shared_ptr<int> sp1(new int(5), StateDeleter());
+        sp1 = std::make_shared<int>(3); // 同样会调用自定义的deleter
+    }
+
+    std::cout << std::endl;
+}
 
 int main(int argc, char *argv[]) {
     std::cout << "\n  ****** begin smart pointer ****** " << std::endl;
 
-    std::cout << "1. shared_ptr" << std::endl;
-    SharedPtr();
+    // std::cout << "1. shared_ptr" << std::endl;
+    // SharedPtr();
 
     // std::cout << "2. weak_ptr" << std::endl;
     // WeakPtr();
 
-    // std::cout << "3. unique_ptr" << std::endl;
-    // UniquePtr();
+    std::cout << "3. unique_ptr" << std::endl;
+    UniquePtr();
 
     std::cout << "\n  ****** end smart pointer ****** " << std::endl;
 
